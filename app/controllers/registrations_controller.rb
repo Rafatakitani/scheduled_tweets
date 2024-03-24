c7a3bd0dce54a96b1812ff9ecef733956da25f01
Rafael Takitani <rafaeltakitani@gmail.com>
2024-03-24 15:26:21 -0300

    v5

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,11 +7,13 @@ class RegistrationsController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: "Succesfully created account"
+      redirect_to root_path, notice: "Successfully created account"
     else
       render :new
     end
   end
+
+  private
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
